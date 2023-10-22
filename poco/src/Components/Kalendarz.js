50baf4fadd7583919ee4645a1bcd528da540f01e
malec <malec@ARTUR_MAECKI>
2023-10-22 19:59:15 +0200

    PC-9 <Kalendarz bez css'ow i logiki>

--- poco/src/Components/Kalendarz.js
+++ poco/src/Components/Kalendarz.js
@@ -1,3 +1,13 @@
-export default function Kalendarz() {
-    return <h1>Kalendarz</h1>
+import * as React from 'react';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
+import "../Models/Kalendarz.css";
+
+export default function StaticDatePickerLandscape() {
+    return (
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <StaticDatePicker orientation="portrait" className='xd large-calendar' />
+        </LocalizationProvider>
+    );
 }
