34324d6ee06ef13c6fbdc10b24a8f1f6c08526ea
malec <malec@ARTUR_MAECKI>
2023-10-26 23:09:57 +0200

    PC-10 <Wyodrębnienie komentarzy + dodanie kalfelka cena balans + logika>

--- poco/src/Components/Kalendarz.js
+++ poco/src/Components/Kalendarz.js
@@ -2,30 +2,53 @@ import * as React from 'react';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import FilledInput from '@mui/material/FilledInput';
+import InputAdornment from '@mui/material/InputAdornment';
+import Button from '@mui/material/Button';
 import "../Models/Kalendarz.css";
 
 export default function StaticDatePickerLandscape() {
-    // Stan do przechowywania komentarzy
     const [comments, setComments] = React.useState({});
-    
-    // Stan do przechowywania wybranego dnia
     const [selectedDate, setSelectedDate] = React.useState(null);
-
-    // Stan do przechowywania komentarza
     const [comment, setComment] = React.useState('');
+    const [price, setPrice] = React.useState(0);
+    const [balance, setBalance] = React.useState(0);
 
-    // Funkcja do obsługi zmiany wybranego dnia
     const handleDateChange = (date) => {
         setSelectedDate(date);
         setComment(comments[date] || '');
     };
 
-    // Funkcja do aktualizacji komentarzy
     const updateComment = () => {
         setComments({
             ...comments,
             [selectedDate]: comment,
         });
+    };
+
+    const handlePriceChange = (e) => {
+        const newPrice = parseFloat(e.target.value) || 0;
+        setPrice(newPrice);
+        updateBalance();
+    };
+
+    const updateBalance = () => {
+        const newBalance = price - calculateTotalCommentsCost();
+        setBalance(newBalance);
+    };
+
+    const calculateTotalCommentsCost = () => {
+        const commentDates = Object.keys(comments);
+        let totalCost = -1;
+
+        for (const date of commentDates) {
+            const commentPrice = parseFloat(comments[date]) || 0;
+            totalCost += commentPrice;
+        }
+
+        return totalCost;
     };
 
     return (
@@ -44,14 +67,34 @@ export default function StaticDatePickerLandscape() {
                         )}
                     />
                 </LocalizationProvider>
+            </div>
+            <div className="comments-container">
                 <div className="comments">
-                    <h2>Komentarz</h2>
-                    <textarea
-                        value={comment}
-                        onChange={(e) => setComment(e.target.value)}
-                        onBlur={updateComment}
-                        placeholder="Dodaj komentarz..."
+                    <FormControl fullWidth sx={{ m: 1 }} variant="filled">
+                        <InputLabel htmlFor="filled-adornment-amount">Dodaj komentarz</InputLabel>
+                        <FilledInput
+                            id="filled-adornment-amount"
+                            startAdornment={<InputAdornment position="start">PLN</InputAdornment>}
+                            value={comment}
+                            onChange={(e) => setComment(e.target.value)}
+                        />
+                        <Button variant="contained" color="primary" onClick={updateComment}>
+                            Dodaj
+                        </Button>
+                    </FormControl>
+                </div>
+            </div>
+            <div className="sidebar">
+                <div className="price">
+                    <h2>Cena: </h2>
+                    <input
+                        type="number"
+                        value={price}
+                        onChange={handlePriceChange}
                     />
+                </div>
+                <div className="balance">
+                    <h2>Balans: {balance}</h2>
                 </div>
             </div>
         </div>
